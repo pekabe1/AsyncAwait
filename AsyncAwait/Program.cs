48410b87201261964cb246cc9fd3557c7a7566e2
k.ziarkowski <k.ziarkowski@PK3-3>
2019-10-03 20:13:01 +0200

    Add project files.

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(a => { Console.WriteLine("ala ma kota"); });
+            t.Start();
+            //  Thread.Sleep(3000);
+            Task task = new Task(() => Console.WriteLine("Hello word"));
+            Task task2 = new Task(() =>
+            {
+                Thread.Sleep(200);
+                Console.WriteLine("word hello");
+            });
+
+            task.Start();
+            task2.Start();
+            Task.WaitAll(task);
+            Task.WhenAll(task, task2);
+            PrintOK().Wait();
+
+            Console.Read();
+            // Console.WriteLine("Hello World!");
+        }
+
+        public static async Task PrintOK()
+        {
+            await Task.Run(() => Console.WriteLine("OK"));
+
+        }
+
+    }
+}
